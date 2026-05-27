b3fc7aa407523befb0dffc0e798dbb7d62e6235f
manque-123 <ge.manque@duocuc.cl>
2026-05-26 22:18:32 -0400

    Finalizando infraestructura y pipeline CI/CD

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Para que el Front pueda leerlo
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({ mensaje: "✅ Conexión Exitosa: Backend Innovatech respondiendo" }));
+});
+server.listen(5000, () => console.log('Backend en puerto 5000'));
